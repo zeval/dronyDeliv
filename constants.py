422b2721eefb6709651e86e075d6d1dafb2a5e92
DiogoS7 <57680051+DiogoS7@users.noreply.github.com>
2019-11-18 19:37:15 +0000

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,30 @@
+# 2019-2020 Programação 1 (LTI)
+# Grupo 16
+# 55375 Diogo Santos
+# 55373 José Almeida
+
+# This module is used for the recording of the constants used throughout the application
+
+# Value of the index number corresponding the name of a drone in file 
+name = 0
+
+# Value of the index number corresponding the name of the operation zone in file
+OperationZone = 1
+
+# Value of the index number corresponding the maximum weight carryable by a drone in file
+MaxWeight = 2
+
+# Value of the index number corresponding the maximum distance a drone in file can travel
+MaxDistance = 3
+
+# Value of the index number corresponding the accumulated distance a drone in file has travelled
+AccumDistance = 4
+
+# Value of the index number corresponding the autonomy of a drone in file
+Autonomy = 5
+
+# Value of the index number corresponding the date when a deliver has been completed and the drone in file has returned to base 
+AvailableDate = 6
+
+# Value of the index number corresponding the hour when a deliver has been completed and the drone in file has returned to base
+AvailableHour = 7
