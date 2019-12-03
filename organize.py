f281095ff8962e2d89fcad4afddc593ef999d31d
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2019-12-03 20:55:25 +0000

    Updated organize.py

--- organize.py
+++ organize.py
@@ -8,32 +8,46 @@ import constants as c
 import readFiles as r
 import time as t
 
-def droneAssigner(drone_list, parcel): #INCOMPLETE, NEEDS TIME COMPARATOR
+def DroneAutonomySorter(list):
+    list = sorted(list, reverse=True, key=lambda list: list[c.Autonomy])
+    return list
+
+def DroneAccumDistanceSorter(list):
+    list = sorted(list, key=lambda list: list[c.AccumDistance])
+    return list
+
+def DroneNameSorter(list):
+    list = sorted(list, key=lambda list: list[c.Name])
+    return list
+
+def droneAssigner(drone_list, parcel):
     """
-    Has to be used once per parcel
+    
     """
     possible_drones = []
     for drone in drone_list:
-        if parcel[c.OperationZone] in drone: # and drone[c.MaxDistance]>=parcel[4] and drone[c.Autonomy]>=(parcel[4]*2/1000): #if drone operates within the same area, has enough reach and has more autonomy than twice the range needed, it'll be considered
+        if parcel[c.OperationZone] in drone and int(drone[c.MaxDistance])>=int(parcel[4]) and int(drone[c.Autonomy])>=int(parcel[4])*2/1000 and int(drone[c.MaxWeight])>=int(parcel[5]):
             possible_drones.append(drone)
-        else:
-            return None
-    
-    possible_drones = sorted(possible_drones, key=lambda possible_drones: possible_drones[c.AvailableHour])
-    
+    if len(possible_drones)==0:
+        return None
+    if len(possible_drones)==1:
+        return possible_drones[0]
+    possible_drones = sorted(possible_drones, key=lambda possible_drones: possible_drones[c.AvailableHour]) #NON FUNCTIONAL TIME SORTER
+    #print(possible_drones)
     if possible_drones[0][c.AvailableHour]==possible_drones[1][c.AvailableHour] and possible_drones[0][c.AvailableDate]==possible_drones[1][c.AvailableDate]: 
-        possible_drones = sorted(possible_drones, reverse=True, key=lambda possible_drones: possible_drones[c.Autonomy])
+        possible_drones = possible_drones[0] + possible_drones[1]
+        possible_drones = DroneAutonomySorter(possible_drones)
         if possible_drones[0][c.Autonomy]==possible_drones[1][c.Autonomy]:
-            possible_drones = sorted(possible_drones, key=lambda possible_drones: possible_drones[c.AccumDistance])
+            possible_drones = DroneAccumDistanceSorter(possible_drones)
             if possible_drones[0][c.AccumDistance]==possible_drones[1][c.AccumDistance]:
-                possible_drones = sorted(possible_drones, key=lambda possible_drones: possible_drones[c.Name])
-    right_drone = possible_drones[0][0]
+                possible_drones = DroneNameSorter(possible_drones)
+    right_drone = possible_drones[0]
     return right_drone
 
 
 fileDict = r.fileFinder()
 droneList = r.droneLister(fileDict["droneFile"])
 parcelList = r.parcelLister(fileDict["parcelFile"])
-parcel = parcelList[0]
+parcel = parcelList[1]
 print(droneAssigner(droneList, parcel))
-    
+
