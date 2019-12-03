99cfc019dd6bdb0d5beaa333499dbfe06019c901
DiogoS7 <57680051+DiogoS7@users.noreply.github.com>
2019-12-03 18:23:57 +0000

    Updated time.py
    Made time.py

--- time.py
+++ time.py
@@ -1,5 +1,36 @@
-preciso que cries uma função que seja capaz de comparar dois argumentos no formato "12h32" e consiga dizer qual é o mais cedo (mais pequeno) 
+#preciso que cries uma função que seja capaz de comparar dois argumentos no formato "12h32" e consiga dizer qual é o mais cedo (mais pequeno) 
 
-isto é capaz de te ajudar https://stackoverflow.com/questions/1831410/python-time-comparison/9716793
+# 2019-2020 Programação 1 (LTI)
+# Grupo 16
+# 55373 José Almeida
+# 55375 Diogo Santos
 
-also, acho que estou a entender este módulo que vais fazer bem, portanto se tiveres alguma duvida pergunta
+import constants
+import readFiles as r
+import datetime
+
+def time_compare(time1, time2):
+    """
+    Compares two time string arguments and returns whichever is earlier (smaller)
+    Requires: both time values to be in the format of a string 
+    Ensures: earliest of both time strings 
+    """
+
+    time1 = datetime.datetime.strptime(str(time1), '%H:%M')
+    time2 = datetime.datetime.strptime(str(time2), '%H:%M')
+
+    if time1.time() < time2.time():
+        
+        return (str(time1.hour)+":"+str(time1.minute))
+    else:
+        return (str(time2.hour)+":"+str(time2.minute))
+
+
+fileDict = r.fileFinder()
+
+valuesd = r.droneLister(fileDict["droneFile"])
+
+time1 = valuesd[1][7]
+time2 = valuesd[0][7]
+
+print(time_compare(time1, time2))
