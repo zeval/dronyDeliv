63f518c98c90195db069b18846c318f92f5b60ea
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2019-12-05 22:41:23 +0000

    Updated time.py \ updated organize.py

--- organize.py
+++ organize.py
@@ -3,10 +3,10 @@
 # 55373 José Almeida
 # 55375 Diogo Santos
 
-
+from __future__ import absolute_import
+import Time as t
 import constants as c
 import readFiles as r
-import time as t
 
 def DroneAutonomySorter(list):
     list = sorted(list, reverse=True, key=lambda list: list[c.Autonomy])
@@ -29,10 +29,10 @@ def droneAssigner(drone_list, parcel):
         if parcel[c.OperationZone] in drone and int(drone[c.MaxDistance])>=int(parcel[4]) and int(drone[c.Autonomy])>=int(parcel[4])*2/1000 and int(drone[c.MaxWeight])>=int(parcel[5]):
             possible_drones.append(drone)
     if len(possible_drones)==0:
-        return None
+        return "Cancelled"
     if len(possible_drones)==1:
         return possible_drones[0]
-    possible_drones = sorted(possible_drones, key=lambda possible_drones: possible_drones[c.AvailableHour]) #NON FUNCTIONAL TIME SORTER
+    possible_drones = t.time_sorter(possible_drones) #NON FUNCTIONAL TIME SORTER
     #print(possible_drones)
     if possible_drones[0][c.AvailableHour]==possible_drones[1][c.AvailableHour] and possible_drones[0][c.AvailableDate]==possible_drones[1][c.AvailableDate]: 
         possible_drones = [possible_drones[0], possible_drones[1]]
@@ -43,12 +43,13 @@ def droneAssigner(drone_list, parcel):
             if possible_drones[0][c.AccumDistance]==possible_drones[1][c.AccumDistance]:
                 possible_drones = DroneNameSorter(possible_drones)
     right_drone = possible_drones[0]
+    # right_drone = used_drones[right_drone] \\\\\\\\\\\ testar se o drone já foi e voltou e poderá estar disponivel \\\\\\ adicionar for das parcelas
     return right_drone
 
 
 fileDict = r.fileFinder()
 droneList = r.droneLister(fileDict["droneFile"])
 parcelList = r.parcelLister(fileDict["parcelFile"])
-parcel = parcelList[4]
+parcel = parcelList[7]
 print(droneAssigner(droneList, parcel))
 
